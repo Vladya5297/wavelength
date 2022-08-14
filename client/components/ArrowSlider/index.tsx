f9d527d4f300e842aff0fe83964e7842c8123f2a
Vladya5297 <123dobro@mail.ru>
2022-08-14 20:04:13 +0300

    feat: final improvements

--- client/components/ArrowSlider/index.tsx
+++ client/components/ArrowSlider/index.tsx
@@ -1,17 +1,35 @@
-import type {State} from 'client/store';
-import {socket} from 'client/utils/socket';
 import React from 'react';
 import {useSelector} from 'react-redux';
+import Slider from 'rc-slider';
+
+import {socket} from 'client/utils/socket';
+import type {State} from 'client/store';
+
+const backgroundColor = '#ec9221';
 
 export const ArrowSlider = () => {
     const angle = useSelector((state: State) => state.angle);
 
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const value = Number(event.target.value);
+    const onChange = (value: number | number[]) => {
         socket.emit('angle', value);
     };
 
     return (
-        <input type="range" min={-90} max={90} step={1} value={angle} onChange={onChange} />
+        <Slider
+            min={-90}
+            max={90}
+            value={angle}
+            onChange={onChange}
+            handleStyle={{
+                height: 28,
+                width: 28,
+                marginTop: -9,
+                backgroundColor,
+                border: 'none',
+                boxShadow: 'none',
+            }}
+            trackStyle={{backgroundColor, height: 10}}
+            railStyle={{height: 10}}
+        />
     );
 };
--- scripts/buildClient.js
+++ scripts/buildClient.js
@@ -26,7 +26,7 @@ const watch = process.argv[2];
 esbuild.build({
     entryPoints: [path.join(clientDirName, 'index.tsx')],
     outfile: path.join(buildDirName, 'index.js'),
-    loader: {'.png': 'file', '.svg': 'file'},
+    loader: {'.png': 'file', '.svg': 'file', '.ttf': 'file'},
     bundle: true,
     sourcemap: true,
     watch: watch ? {
